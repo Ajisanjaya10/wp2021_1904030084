793bfe095f659beb14bbc7d2d473daf50472c041
Ajisanjaya10 <ajisanjaya295@gmail.com>
2022-01-26 10:43:39 +0700

    tugas pertemuan10

---
+++ Pertemuan10/koneksi.php
@@ -0,0 +1,19 @@
+<?php
+
+$host = 'localhost';
+$user = 'root';
+$pass = '';
+$db   = 'wprog5';
+
+$conn = mysqli_connect($host, $user, $pass, $db); 
+
+$result = mysqli_query($conn, 'SELECT * FROM calon_mhs');
+
+// AMBIL DATA DARI TABEL CALON MHS
+//mysqli_fetch_row();
+//mysqli_fetch_assoc();
+//mysqli_fetch_array();
+
+mysqli_fetch_row($result);
+var_dump(mysqli_fetch_row($result));
+ ?>
